dfa4b13f6189320af9934098b2a4c767fe8cb9ca
Wahid Maruf <wahid.maruf@ibm.com>
2022-06-05 00:50:31 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# flutter_piano
+
+A Simple Flutter App for Business Card
+
+## Description
+
+Flutter project for playing music
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text("Hello World")
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
